29bbf93c7b86f15e3ee195cca5befcaeb9f9452f
Hai Trieu <trieuqn@gmail.com>
2019-12-30 09:23:53 +0700

    Fix regex url

--- ActiveLabel/RegexParser.swift
+++ ActiveLabel/RegexParser.swift
@@ -12,9 +12,7 @@ struct RegexParser {
 
     static let hashtagPattern = "(?:^|\\s|$)#[\\p{L}0-9_]*"
     static let mentionPattern = "(?:^|\\s|$|[.])@[\\p{L}0-9_]*"
-    static let urlPattern = "(^|[\\s.:;?\\-\\]<\\(])" +
-        "((https?://|www\\.|pic\\.)[-\\w;/?:@&=+$\\|\\_.!~*\\|'()\\[\\]%#,☺]+[\\w/#](\\(\\))?)" +
-    "(?=$|[\\s',\\|\\(\\).:;?\\-\\[\\]>\\)])"
+    static let urlPattern = "(http|https)?://([-\\w\\.]+)+(:\\d+)?(/([\\w/_\\.]*(\\?\\S+)?)?)?"
 
     private static var cachedRegularExpressions: [String : NSRegularExpression] = [:]
 
